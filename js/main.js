7f1f082b69f23db22e4c7cb7f4384f0481d7fcaa
Jhan Carlos MC <jhanc.martinezc@uqvirtual.edu.co>
2023-09-26 10:13:46 -0500

    Inicios JS

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const cadena = 'Texto mas 234';
+const numeros = -22.0;
+const logicos = false;
+
+let string = "Texto dos";
+let int = 100;
+let boolean = true;
+
+
+let numeroUno = 20;
+let numeroDos = 10;
+
+if (numeroUno > numeroDos) {
+    console.log("El numero mayor es " + numeroUno + "(Numero Uno)");
+} else if (numeroDos > numeroUno) {
+    console.log("El numero mayor es " + numeroDos + "(Numero Dos)");
+} else {
+    console.log("Los numeros son iguales");
+}
+
+if (false) {
+    
+} else if(true){
+    
+} else if(false){
+
+}
