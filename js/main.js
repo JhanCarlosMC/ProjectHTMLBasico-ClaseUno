7fa959dbc88df660f299aadf4b0a7839cd28edd0
Jhan Carlos MC <jhanc.martinezc@uqvirtual.edu.co>
2023-09-26 15:44:36 -0500

    Condicionales y Bucles en JS

--- js/main.js
+++ js/main.js
@@ -1,27 +1,128 @@
-const cadena = 'Texto mas 234';
-const numeros = -22.0;
-const logicos = false;
+//-----------------Condicionales
 
-let string = "Texto dos";
-let int = 100;
-let boolean = true;
+//comparacionNumeros(2, 3);
 
+//Leer dos números y comparar si son iguales.
+function comparacionNumeros(num1, num2) {
 
-let numeroUno = 20;
-let numeroDos = 10;
-
-if (numeroUno > numeroDos) {
-    console.log("El numero mayor es " + numeroUno + "(Numero Uno)");
-} else if (numeroDos > numeroUno) {
-    console.log("El numero mayor es " + numeroDos + "(Numero Dos)");
-} else {
-    console.log("Los numeros son iguales");
+    if (num1 === num2) {
+        console.log("Los numeros ingresados son iguales");
+    } else {
+        console.log("Los numeros ingresados NO son iguales");
+    }
 }
 
-if (false) {
-    
-} else if(true){
-    
-} else if(false){
+//diaSemana(1);
 
+function diaSemana(valorDia) {
+
+    switch (valorDia) {
+        case 1:
+            console.log("Lunes");
+            break;
+
+        case 2:
+            console.log("Martes");
+            break;
+
+        case 3:
+            console.log("Miercoles");
+            break;
+
+        default:
+            console.log("Otro dia de la semana");
+            break;
+    }
+}
+
+//diaFinSemana("Miercoles");
+
+function diaFinSemana(dia) {
+
+    switch (dia) {
+        case "Lunes":
+            console.log("Dia entre semana");
+            break;
+
+        case "Martes":
+            console.log("Dia entre semana");
+            break;
+
+        case "Sabado":
+            console.log("Fin de semana");
+            break;
+
+        case "Domingo":
+            console.log("Fin de semana");
+            break;
+        default:
+            console.log("Otro dia entre semana");
+            break;
+    }
+}
+
+//-----------------Bucles
+
+//ejemploWhile();
+function ejemploWhile() {
+    let contador = 1;
+    let flag = false;
+
+    while (flag) {
+        console.log("Contador = " + contador);
+        contador++;
+
+        if (contador === 5) {
+            flag = false;
+        }
+    }
+}
+
+//ejemploDoWile();
+function ejemploDoWile() {
+    let contador = 10;
+
+    do {
+        console.log("Contador = " + contador);
+        contador--;
+    } while (contador >= 10);
+}
+
+//ejemploFor();
+
+function ejemploFor() {
+    for (let i = 0; i < 10; i++) {
+        console.log("Contador = " + i);
+    }
+}
+
+//Leer un número y mostrar de manera descendente 
+//todos los números inferiores a él.
+
+//numDescendentesDoWHile(5);
+
+function numDescendentesDoWHile(numero) {
+
+    do {
+        console.log("DoW " + numero);
+        numero = numero - 1;
+    } while (numero > 0);
+}
+
+//numDescendentesWHile(5);
+
+function numDescendentesWHile(numero) {
+
+    while (numero > 0) {
+        console.log("W " + numero);
+        numero = numero - 1;
+    }
+}
+
+//numDescendentesFor(5);
+function numDescendentesFor(numero) {
+
+    for (let i = numero; i > 0; i--) {
+        console.log("For " + i);
+    }
 }
