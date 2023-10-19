32e92f5d65160bdaed6ecd9098d5efb18021417a
Jhan Carlos MC <jhanc.martinezc@uqvirtual.edu.co>
2023-10-19 10:53:39 -0500

    Ejemplos De Login y Registro

--- ejemplos/js/producto.js
+++ ejemplos/js/producto.js
@@ -32,3 +32,49 @@ function agregarProductoCarrito(idProducto) {
     newProductoCarrito.innerHTML = `<strong> Nombre del producto:</strong> ${nombreProducto} <br><strong>Precio:</strong> $${precioProducto}`;
     carritoCompras.appendChild(newProductoCarrito);
 }
+
+//--------------------------Cargar y Guardar IMG---------------------
+const formularioImagen = document.getElementById('formulario-imagen');
+
+formularioImagen.addEventListener('submit', function (e) {
+    e.preventDefault(); // Evita el envío del formulario por defecto
+
+    //Files[0] = contiene la lista de archivos seleccionados.
+    const archivoImagen = document.getElementById('imagen').files[0];
+
+    //Se verifica si el elemento es nulo/null
+    if (archivoImagen) {
+        //Instancia de FileReader para leer el contenido del archivo
+        const lector = new FileReader();
+
+        //Comienza a leer el contenido de la imagen(Una vez leido ejecuta el metodo onload)
+        lector.readAsDataURL(archivoImagen);
+
+        //El evento se activa cuando el archivo se ha cargado correctamente.
+        lector.onload = function (evento) {
+            //Obtenemos los datos del archivo cargado en forma de URL
+            const imagenBase64 = evento.target.result; //Trae los datos en una cadena de caracteres ASCII
+            //Mostramos la URL bsae64
+            console.log(imagenBase64);
+
+            // Almacena la imagen en el localStorage
+            localStorage.setItem('imagen', imagenBase64);
+        };
+
+        alert('Imagen almacenada correctamente');
+
+    } else {
+        alert('Por favor, seleccione una imagen antes de enviar el formulario.');
+    }
+});
+
+function cargarImgagen() {
+    // Recuperar y mostrar la imagen al cargar la página
+    const imagenMostrada = document.getElementById('imagen-mostrada');
+    const imagenAlmacenada = localStorage.getItem('imagen');
+
+    if (imagenAlmacenada) {
+        imagenMostrada.src = imagenAlmacenada;
+    }
+}
+	
--- js/login.js
+++ js/login.js
@@ -1,8 +1,29 @@
 const form = document.getElementById("login");
+let listUsuarios = JSON.parse(localStorage.getItem("listUsuarios"));
+
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
-    window.location.href = "/index.html";
+    const emailValidar = document.getElementById("email").value;
+    const passwordValidar = document.getElementById("password").value;
+    let usuarioObtenido;
+
+    listUsuarios.forEach(usuario => {
+
+        if (usuario.email === emailValidar) {
+            usuarioObtenido = usuario;
+        }
+    });
+
+    if (usuarioObtenido) {
+        if (usuarioObtenido.password === passwordValidar) {
+            window.location.href = "/index.html";
+        } else {
+            alert("La constraseña es incorrecta");
+        }
+    }else{
+        alert("El correo no fue encontrado");
+    }
 
 });
 
